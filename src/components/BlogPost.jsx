f603999826dc95484ac7f2b9529d20fad1262829
Suleman615 <ghlmsuleman@gmail.com>
2024-07-28 16:06:58 +0500

    Hero Section updated

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -3,7 +3,7 @@ const BlogPost = ({image,text,link}) => {
   return (
     <div className='position-relative mb-5 px-3 '>
         <img className='w-100' src={image} alt="Blog Cover"  />
-        <div className='position-absolute d-flex flex-column align-align-items-center justify-content-between w-75 background-light  rounded-3 shadow-lg py-2 px-4 ' style={{bottom:'-15%', left:'12%', height:'130px'}}>
+        <div className='position-absolute d-flex flex-column align-align-items-center justify-content-between w-75 background-light  rounded-3 shadow py-2 px-4 ' style={{bottom:'-15%', left:'12%',  minHeight:'130px'}}>
             <p className='text-grey fw-bold text-center'>{text}</p>
             <button className='border-0 background-light text-green fw-semibold mx-auto'>Read More <img src={arrow_right} alt="Arrow"  /></button>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,50 @@ import hero from '../assets/hero.svg'
 
 const Hero = () => {
     return (
-        <div className='background-light p-5 '>
+        <div className='background-light py-5 px-2 px-sm-5 pb-0 '>
 
-            <div className='d-flex flex-column-reverse flex-md-row container py-5 '>
-                <div className=' flex-grow-1 w-full gap-4 d-flex flex-column justify-content-center pt-5'>
-                    <h1 className=' text-dark-grey' style={{fontSize:'8ex'}}>Lessons and insights <br/><span className='text-green'>from 8 years</span></h1>
-                    <p className='fw-semibold text-grey' style={{fontSize:'16px'}}>Where to grow your business as a photographer: site or social media?</p>
-                    <button className='rounded border-0 background-green text-white px-4 py-2' style={{width:'fit-content'}} >Register</button>
+            
+
+
+            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active slider-button" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2" className='slider-button'></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3" className='slider-button'></button>
                 </div>
-                <div className=' flex-grow-1 '><img style={{width:'100%'}} src={hero} alt="HERO Image"  /></div>
+                <div className="carousel-inner ">
+                    <div className="carousel-item active">
+                        <div className='d-flex flex-column-reverse flex-md-row container py-5 '>
+                            <div className=' flex-grow-1 w-full gap-4 d-flex flex-column justify-content-center pt-5'>
+                                <h1 className=' text-dark-grey' style={{ fontSize: '8ex' }}>Lessons and insights <br /><span className='text-green'>from 8 years</span></h1>
+                                <p className='fw-semibold text-grey' style={{ fontSize: '16px' }}>Where to grow your business as a photographer: site or social media?</p>
+                                <button className='rounded border-0 background-green text-white px-4 py-2' style={{ width: 'fit-content' }} >Register</button>
+                            </div>
+                            <div className=' flex-grow-1 '><img style={{ width: '100%' }} src={hero} alt="HERO Image" /></div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div className='d-flex flex-column-reverse flex-md-row container py-5 '>
+                            <div className=' flex-grow-1 w-full gap-4 d-flex flex-column justify-content-center pt-5'>
+                                <h1 className=' text-dark-grey' style={{ fontSize: '8ex' }}>Lessons and insights <br /><span className='text-green'>from 8 years</span></h1>
+                                <p className='fw-semibold text-grey' style={{ fontSize: '16px' }}>Where to grow your business as a photographer: site or social media?</p>
+                                <button className='rounded border-0 background-green text-white px-4 py-2' style={{ width: 'fit-content' }} >Register</button>
+                            </div>
+                            <div className=' flex-grow-1 '><img style={{ width: '100%' }} src={hero} alt="HERO Image" /></div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div className='d-flex flex-column-reverse flex-md-row container py-5 '>
+                            <div className=' flex-grow-1 w-full gap-4 d-flex flex-column justify-content-center pt-5'>
+                                <h1 className=' text-dark-grey' style={{ fontSize: '8ex' }}>Lessons and insights <br /><span className='text-green'>from 8 years</span></h1>
+                                <p className='fw-semibold text-grey' style={{ fontSize: '16px' }}>Where to grow your business as a photographer: site or social media?</p>
+                                <button className='rounded border-0 background-green text-white px-4 py-2' style={{ width: 'fit-content' }} >Register</button>
+                            </div>
+                            <div className=' flex-grow-1 '><img style={{ width: '100%' }} src={hero} alt="HERO Image" /></div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
         </div>
